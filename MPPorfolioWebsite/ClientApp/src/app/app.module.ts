8ea771e14effddb7871065a6523dee3cb01d99bb
Marcell <Marcell.Pather@dmc.co.za>
2021-10-24 13:43:07 +0200

    Added Contact, Portfolio and Services Pages

--- MPPorfolioWebsite/ClientApp/src/app/app.module.ts
+++ MPPorfolioWebsite/ClientApp/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TopNavMenuComponent } from './top-nav-menu/top-nav-menu.component';
 import { SkillsComponent } from './skills/skills.component';
 import { CarouselComponent } from './carousels/carousel.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { CarouselComponent } from './carousels/carousel.component';
     FetchDataComponent,
     TopNavMenuComponent,
     SkillsComponent,
-    CarouselComponent
+    CarouselComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -35,6 +41,9 @@ import { CarouselComponent } from './carousels/carousel.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'contact', component: ContactComponent },
     ])
   ],
   providers: [],
